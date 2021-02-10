5cee527b495500a0b85e5e8b7d03ebb0faa1b20a
Remi Chavance <aeriio99@gmail.com>
2021-02-10 20:19:46 +0100

    delete base connection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,17 +13,26 @@ export class AppComponent {
   constructor() {
     
     // Firebase access
+    
+    /**
+     * /!\ Initialiser la connection a la base de données
+     */
+
+    /*
+
     const firebaseConfig = {
-      apiKey: "AIzaSyC1zspIj9wrsBiQX16g20kEfWJ0C-oGqDI",
+      apiKey: "",
       authDomain: "projet-s4-ghac.firebaseapp.com",
       databaseURL: "https://projet-s4-ghac-default-rtdb.firebaseio.com",
       projectId: "projet-s4-ghac",
-      storageBucket: "projet-s4-ghac.appspot.com",
-      messagingSenderId: "378917706292",
-      appId: "1:378917706292:web:00d412b6082bb39b0fc861"
+      storageBucket: "",
+      messagingSenderId: "",
+      appId: ""
     };
 
-    // Initialize Firebase
     firebase.default.initializeApp(firebaseConfig);
+
+    */
+
   }
 }
