e4747e57710025c87ebf158133ec3d76c769e7ee
Amandine GROS <amandine.gros@gmail.com>
2021-02-11 10:29:45 +0100

    mon-premier-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { User } from './models/user.model';
 import * as firebase from 'firebase';
 
 @Component({
@@ -7,27 +6,30 @@ import * as firebase from 'firebase';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'projet';
 
   constructor() {
     
-    // Copy / Past this from Firebase database
-
-    // Firebase access
+    
+    // Your web app's Firebase configuration
     const firebaseConfig = {
-      apiKey: ",
-      authDomain: "projet-s4-ghac.firebaseapp.com",
-      databaseURL: "https://projet-s4-ghac-default-rtdb.firebaseio.com",
-      projectId: "projet-s4-ghac",
-      storageBucket: "projet-s4-ghac.appspot.com",
-      messagingSenderId: "378917706292",
-      appId: "1:378917706292:web:00d412b6082bb39b0fc861"
+      apiKey: "AIzaSyC20shHVQ2ja13Ll_cWrkHOosku1SgBlM0",
+      authDomain: "projet-s4-7081d.firebaseapp.com",
+      databaseURL: "https://projet-s4-7081d-default-rtdb.firebaseio.com",
+      projectId: "projet-s4-7081d",
+      storageBucket: "projet-s4-7081d.appspot.com",
+      messagingSenderId: "1084615419975",
+      appId: "1:1084615419975:web:9a6172788b02accdd52962"
     };
-
     // Initialize Firebase
     firebase.default.initializeApp(firebaseConfig);
-  }
+
+
+    //test github !!
 
   }
+
 }
+
