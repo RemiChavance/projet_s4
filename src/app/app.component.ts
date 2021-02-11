e431f42dce07f365ead0594d5896340738aa38ad
Remi Chavance <aeriio99@gmail.com>
2021-02-11 10:22:46 +0100

    first commit devRemi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,22 +10,20 @@ export class AppComponent {
   title = 'projet';
 
   constructor() {
-    
-    // Your web app's Firebase configuration
-      const firebaseConfig = {
-      apiKey: ",
-      authDomain: "projet-s4-385ad.firebaseapp.com",
-      databaseURL: "https://projet-s4-385ad-default-rtdb.firebaseio.com",
-      projectId: "projet-s4-385ad",
-      storageBucket: "projet-s4-385ad.appspot.com",
-      messagingSenderId: "527248466640",
-      appId: "1:527248466640:web:59dec3afeb644c5773b4d5"
+
+    // Firebase access
+    const firebaseConfig = {
+      apiKey: "AIzaSyC1zspIj9wrsBiQX16g20kEfWJ0C-oGqDI",
+      authDomain: "projet-s4-ghac.firebaseapp.com",
+      databaseURL: "https://projet-s4-ghac-default-rtdb.firebaseio.com",
+      projectId: "projet-s4-ghac",
+      storageBucket: "projet-s4-ghac.appspot.com",
+      messagingSenderId: "378917706292",
+      appId: "1:378917706292:web:00d412b6082bb39b0fc861"
     };
 
     // Initialize Firebase
     firebase.default.initializeApp(firebaseConfig);
-    
   }
+    
 }
-
-// test github
