0f6884606396bca59cf683ba209f065c8f89058f
philippe <philippaudier@gmail.com>
2021-02-18 16:40:16 +0100

    heroku server.js update 2

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const PORT = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/dist'));
 
-app.listen(process.env.PORT || 8080);
+
+app.listen(PORT);
 
 // PathLocationStrategy
 app.get('/*', function(req, res) {
