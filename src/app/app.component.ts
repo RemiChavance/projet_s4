f19f99ae98f236230d3fa461d0ff27288e604398
philippe <philippaudier@gmail.com>
2021-02-18 16:14:06 +0100

    Initial Heroku commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,13 +13,13 @@ export class AppComponent {
 
     // Firebase access
     const firebaseConfig = {
-      apiKey: "AIzaSyC1zspIj9wrsBiQX16g20kEfWJ0C-oGqDI",
-      authDomain: "projet-s4-ghac.firebaseapp.com",
-      databaseURL: "https://projet-s4-ghac-default-rtdb.firebaseio.com",
-      projectId: "projet-s4-ghac",
-      storageBucket: "projet-s4-ghac.appspot.com",
-      messagingSenderId: "378917706292",
-      appId: "1:378917706292:web:00d412b6082bb39b0fc861"
+      apiKey: 'AIzaSyDZlLlmtcdyxgBnQvwFn44F4xiLu7rFcwU',
+      authDomain: 'projet-s4-4e77d.firebaseapp.com',
+      databaseURL: 'https://projet-s4-4e77d-default-rtdb.firebaseio.com',
+      projectId: 'projet-s4-4e77d',
+      storageBucket: 'projet-s4-4e77d.appspot.com',
+      messagingSenderId: '651198091582',
+      appId: '1:651198091582:web:0e1cacf4a96f64af8d5c6f'
     };
 
     // Initialize Firebase
