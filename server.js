d3533837e5f9f79a4908fefe0de3d9cc33f3544f
Remi Chavance <aeriio99@gmail.com>
2021-02-19 15:04:34 +0100

    heroku commit

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.listen(PORT);
 // PathLocationStrategy
 app.get('/*', function(req, res) {
 
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/projet/index.html'));
 
 })
 
