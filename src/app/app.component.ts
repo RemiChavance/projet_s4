61643b79fba8711bae5bd75c1073b03f07792cec
Remi Chavance <aeriio99@gmail.com>
2021-05-18 11:26:30 +0200

    fautes orthographe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,17 +12,17 @@ export class AppComponent {
   constructor() {
 
     // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyD0jinb4_fJfj7tiXFpA8ynOtB8VqWoWRE",
-    authDomain: "projet-s4-dev.firebaseapp.com",
-    databaseURL: "https://projet-s4-dev-default-rtdb.firebaseio.com",
-    projectId: "projet-s4-dev",
-    storageBucket: "projet-s4-dev.appspot.com",
-    messagingSenderId: "624618870456",
-    appId: "1:624618870456:web:8fc1bbdc57b297da1a464d"
-  };
-      
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+    const firebaseConfig = {
+      apiKey: "AIzaSyD0jinb4_fJfj7tiXFpA8ynOtB8VqWoWRE",
+      authDomain: "projet-s4-dev.firebaseapp.com",
+      databaseURL: "https://projet-s4-dev-default-rtdb.firebaseio.com",
+      projectId: "projet-s4-dev",
+      storageBucket: "projet-s4-dev.appspot.com",
+      messagingSenderId: "624618870456",
+      appId: "1:624618870456:web:8fc1bbdc57b297da1a464d"
+    };
+        
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
   }
 }
